b7d7ca9132644a4e6f4eee39ec1e1800248e163a
Ron Jensen <ron@jentronics.com>
2021-01-04 15:47:53 -0700

    A badly working chart.js sample. Note that chart.html uses old 1.0 syntax while Chart.min.js is 2.9.4

--- Flask-Application/app/main.py
+++ Flask-Application/app/main.py
@@ -1,8 +1,18 @@
 
 from flask import (Flask, render_template)
+# from flask import Markup
 
 app = Flask(__name__) #Needs to be used in every flask application
 
 @app.route('/')
 def index():
-    return render_template('main/index.html')
+    return render_template('main/index.html', title='Index')
+
+@app.route("/log")
+def chart():
+    labels = ["January","February","March","April","May","June","July","August"]
+    values = [10,9,8,7,6,4,7,8]
+    # make a 'JSON' dictonary
+    data = { 'labels' : labels, 'values': values }
+    return render_template('main/chart.html', data=data, title='Log File')
+
