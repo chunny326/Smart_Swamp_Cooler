843378d99948f653268141e43180b38d2849baba
Jayden Smith <chunny326@gmail.com>
2021-02-27 14:56:09 -0700

    Adding alg tests

--- smart_alg_test.py
+++ smart_alg_test.py
@@ -2,15 +2,16 @@ import libs
 
 def test_desired_temp():
     # Desired Temp, Outside Temp, Inside Temp, Expected Result
-    test_data = [ (75,90,90,"Fan w/Pump"),
-                  (75,90,60,"Fan"),
-                  (75,60,90,"Fan"),
-                  (75,60,60,"Off"), ]
+    test_data = [ {"desired_T": 75, "out_T": 90, "in_T": 90, "out_rh": 30, "in_rh": 30, "result": "Fan w/Pump"},
+                  {"desired_T": 75, "out_T": 90, "in_T": 60, "out_rh": 30, "in_rh": 30, "result": "Fan"},
+                  {"desired_T": 75, "out_T": 60, "in_T": 90, "out_rh": 30, "in_rh": 30, "result": "Fan"},
+                  {"desired_T": 75, "out_T": 60, "in_T": 60, "out_rh": 30, "in_rh": 30, "result": "Off"},] 
+    
     for data in test_data:
-        setting = libs.get_auto_setting(data[1], 30,\
-                                        data[2], 30,\
-                                        desired_temp=data[0])
-        print("Expected: {} \t Received: {}".format(data[3], setting))
+        setting = libs.get_auto_setting(data["out_T"], data["out_rh"],\
+                                        data["in_T"], data["in_rh"],\
+                                        data["desired_T"])
+        print("Expected: {} \t Received: {}".format(data["result"], setting))
         
 if __name__ == "__main__":
     test_desired_temp()
